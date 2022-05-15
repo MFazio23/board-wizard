94512a0ca1ff344c1d19440a60f7b8b47d02191b
Michael Fazio <mfazio23@gmail.com>
2022-05-15 17:21:24 -0500

    Upgrade Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.1.3" apply false
-    id("com.android.library") version "7.1.3" apply false
+    id("com.android.application") version "7.2.0" apply false
+    id("com.android.library") version "7.2.0" apply false
     kotlin("android") version "1.6.10" apply false
     id("dagger.hilt.android.plugin") version "2.41" apply false
 }
